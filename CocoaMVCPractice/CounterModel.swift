1015830520ad8daa8727d6931d248f380c7ab861
TazawaAyumu <tazawaayumu@gmail.com>
2021-04-14 22:15:42 +0900

    InitalCommit

---
+++ CocoaMVCPractice/CounterModel.swift
@@ -0,0 +1,30 @@
+//
+//  CounterModel.swift
+//  CocoaMVCPractice
+//
+//  Created by 田澤歩 on 2021/04/14.
+//
+
+import Foundation
+
+class CountModel {
+    static let notifcationName = "CounterModelChanged"
+    let notificationCounter = NotificationCenter()
+    
+    internal var count: Int = 0 {
+        
+        didSet {
+            
+            notificationCounter.post(name: .init(rawValue: CountModel.notifcationName), object: count)
+        }
+    }
+    
+    func countUp() {
+        count += 1
+    }
+    
+    func countDown() {
+        count -= 1
+    }
+    
+}
