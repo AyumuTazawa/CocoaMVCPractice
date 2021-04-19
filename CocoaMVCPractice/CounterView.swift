8ead259a06f7a063107b6358ab5ec2352ddc55a8
TazawaAyumu <tazawaayumu@gmail.com>
2021-04-19 22:30:43 +0900

    関連付け

---
+++ CocoaMVCPractice/CounterView.swift
@@ -0,0 +1,20 @@
+//
+//  CounterView.swift
+//  CocoaMVCPractice
+//
+//  Created by 田澤歩 on 2021/04/19.
+//
+
+import UIKit
+
+class CounterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
