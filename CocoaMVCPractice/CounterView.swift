496825d20e10f36e24910f31ee76ba0192c51acc
TazawaAyumu <tazawaayumu@gmail.com>
2021-04-24 23:09:58 +0900

    View処理追加

--- CocoaMVCPractice/CounterView.swift
+++ CocoaMVCPractice/CounterView.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class CounterView: UIView {
 
-   
+    @IBOutlet weak var countLabel: UILabel!
+    public func render(count: Int) {
+        countLabel.text = String(count)
+    }
 
 }
